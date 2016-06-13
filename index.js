9c62906a7197980c74ccb6e05c6972e21e1fe4b0
谭显林 <tanxianlin@coocaa.com>
2016-06-13 20:24:31 +0800

    add args-filter

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 module.exports.KoaApplication = require('./lib/koas/KoaApplication');
-module.exports.Clouderr       = require('./lib/errors/Clouderr');
-module.exports.errors         = require('./lib/errors/errors');
-module.exports.KoaMiddle      = require('./lib/koas/KoaMiddles');
-module.exports.Cloudeer       = require('./lib/rpc/Cloudeer');
+module.exports.Clouderr = require('./lib/errors/Clouderr');
+module.exports.errors = require('./lib/errors/errors');
+module.exports.argsFilter = require('./lib/errors/args-filter');
+module.exports.KoaMiddle = require('./lib/koas/KoaMiddles');
+module.exports.Cloudeer = require('./lib/rpc/Cloudeer');
 
 module.exports.mongo = {
-  BaseService   : require('./lib/koas/mongo/BaseService'),
+  BaseService: require('./lib/koas/mongo/BaseService'),
   BaseController: require('./lib/koas/mongo/BaseController')
 };
-module.exports.orm   = {
+module.exports.orm = {
   mongo: require('./lib/orm/mongo'),
   mysql: require('./lib/orm/mysql')
 };
