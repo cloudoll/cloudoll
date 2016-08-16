e7b0e23d99698134a1cd328cdcbcc388adbb6e97
Cloudbeer <cloudbeer@gmail.com>
2016-08-16 19:06:24 +0800

    KoaApplicatiion 不再重试端口了 ObjectId 直接从这个包导出了。

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 module.exports.KoaApplication = require('./lib/koas/KoaApplication');
-module.exports.Clouderr = require('./lib/errors/Clouderr');
-module.exports.errors = require('./lib/errors/errors');
-module.exports.argsFilter = require('./lib/errors/args-filter');
-module.exports.KoaMiddle = require('./lib/koas/KoaMiddles');
-module.exports.Cloudeer = require('./lib/rpc/Cloudeer');
+module.exports.Clouderr       = require('./lib/errors/Clouderr');
+module.exports.errors         = require('./lib/errors/errors');
+module.exports.argsFilter     = require('./lib/errors/args-filter');
+module.exports.KoaMiddle      = require('./lib/koas/KoaMiddles');
+module.exports.Cloudeer       = require('./lib/rpc/Cloudeer');
 
-module.exports.mongo = {
-  BaseService: require('./lib/koas/mongo/BaseService'),
+module.exports.mongo  = {
+  BaseService   : require('./lib/koas/mongo/BaseService'),
   BaseController: require('./lib/koas/mongo/BaseController'),
-  ObjectID    : require("mongodb").ObjectID
+  ObjectID      : require("mongodb").ObjectID
 };
-module.exports.logger= require('tracer').colorConsole();
-module.exports.orm   = {
+module.exports.logger = require('tracer').colorConsole();
+module.exports.orm    = {
   mongo   : require('./lib/orm/mongo'),
   mysql   : require('./lib/orm/mysql'),
   postgres: require('./lib/orm/postgres'),
-  redis:require('./lib/orm/redis')
+  redis   : require('./lib/orm/redis'),
+  ObjectId: require("mongodb").ObjectId
 };
