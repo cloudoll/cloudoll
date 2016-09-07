5937c8c06d6a40a6b50298d4f87d4ac2a5775191
Cloudbeer <cloudbeer@gmail.com>
2016-09-07 18:29:22 +0800

    增加了注册中心的 socket 客户端

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ module.exports.errors         = require('./lib/errors/errors');
 module.exports.argsFilter     = require('./lib/errors/args-filter');
 module.exports.KoaMiddle      = require('./lib/koas/KoaMiddles');
 module.exports.Cloudeer       = require('./lib/rpc/Cloudeer');
+module.exports.tools          = require('./lib/tools');
 
-module.exports.mongo     = {
+module.exports.mongo = {
   BaseService   : require('./lib/koas/mongo/BaseService'),
   BaseController: require('./lib/koas/mongo/BaseController'),
   ObjectID      : require("mongodb").ObjectID
 };
-module.exports.logger    = require('tracer').colorConsole();
 
-module.exports.orm       = {
+module.exports.logger = require('tracer').colorConsole();
+
+module.exports.orm = {
   mongo   : require('./lib/orm/mongo'),
   mysql   : require('./lib/orm/mysql'),
   postgres: require('./lib/orm/postgres'),
   redis   : require('./lib/orm/redis'),
   ObjectID: require("mongodb").ObjectID
 };
-module.exports.thirdpart = {
+
+module.exports.libs = {
   co     : require('co'),
   koa    : require('koa'),
   mongodb: require('mongodb'),
