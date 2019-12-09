abef9df062b75587ce16d77838f80afaf4217cb8
Cloudbeer <19568205@qq.com>
2019-12-09 18:29:41 +0800

    wrapper changed

--- app/middleware/wrapper.js
+++ app/middleware/wrapper.js
@@ -3,13 +3,10 @@ const url = require("url");
 const errors = require("../../lib/eve-errors").errors;
 module.exports = (options) => {
     return async (ctx, next) => {
-        console.log(options);
-
         /*
-        可以配置需要匹配的路径 paths
+        可以配置需要匹配的路径 paths，如果不指定，那么就匹配所有的路径。
         */
         let matched;
-        // options = options || {};
         let paths = options.paths || ['/']; //如果不制定路径，则全部 wrapper
         let parsedUrl = url.parse(ctx.url);
         let pathname = parsedUrl.pathname;
@@ -24,9 +21,6 @@ module.exports = (options) => {
             //这几个远程调用的 url 也需要 wrap
             matched = pathname === "/rpc/eve" || pathname === "/rpc/get-eve" || pathname === "/rpc/post-eve";
         }
-
-        // console.log('matched?', matched, 'Now status:', ctx.status);
-        // console.log('opt', ctx.options);
         if (matched) {
             const response = new Response({traceId: "12314"});
             try {
@@ -54,17 +48,5 @@ module.exports = (options) => {
         } else {
             await next();
         }
-
-        // console.log('Now Status:', ctx.status);
-        // const response = new Response(ctx.options);
-        // let res = response.body(ctx.body);
-        // let jpCallback = ctx.request.query.callback;
-        // if (jpCallback && ctx.method === "GET") {
-        //     ctx.type = "text/javascript";
-        //     ctx.body = `${jpCallback}(${JSON.stringify(res)})`;
-        // } else {
-        //     ctx.body = res;
-        //     console.log('After Status:', ctx.status);
-        // }
     }
 }
